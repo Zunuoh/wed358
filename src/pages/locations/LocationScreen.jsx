eea362e88e92209e601740b96543c3fde59bf1f8
Zunuoh <audponu@gmail.com>
2022-11-24 20:35:58 +0000

    inserted map image

--- src/pages/locations/LocationScreen.jsx
+++ src/pages/locations/LocationScreen.jsx
@@ -23,19 +23,19 @@ const LocationScreen = () => {
       <div className='mapContainer'>
             <div className='locationDetails'>
               <div style={{fontSize:25}}>Location & Informations</div>
-              <div style={{display:"flex", marginTop:5}}>
+              <div style={locationDetails}>
                 <div><FeatherIcon icon="map-pin" size="15" /></div>
                 <div className='locationDetailsText'>Vineyard at Pier 26, Hudson river park, New York</div>
               </div>
-              <div style={{display:"flex", marginTop:5}}>
+              <div style={locationDetails}>
                 <div><FeatherIcon icon="phone" size="15" /></div>
                 <div className='locationDetailsText'>+ 00 123 456 7</div>
               </div>
-              <div style={{display:"flex", marginTop:5}}>
+              <div style={locationDetails}>
                 <div><FeatherIcon icon="mail" size="15" /></div>
                 <div className='locationDetailsText'>bridge@example.com</div>
               </div>
-              <div style={{display:"flex", marginTop:5}}>
+              <div style={locationDetails}>
                 <div><FeatherIcon icon="instagram" size="15" /></div>
                 <div className='locationDetailsText'>#janeandarthurwedding</div>
               </div>
@@ -43,7 +43,7 @@ const LocationScreen = () => {
               <div></div>
             </div>
             <div className='locationMap'>
-              {/* <div className='map'>ff</div> */}
+              <img src="https://images.unsplash.com/photo-1586449480558-33ae22ffc60d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z29vZ2xlJTIwbWFwfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt='' style={locationMapImage}/>
             </div>
 
       </div>
@@ -57,6 +57,9 @@ const LocationScreen = () => {
     </div>
   )
 }
+
+const locationDetails = {display:"flex", marginTop:5}
+const locationMapImage = {width:'40vw', height:'40vh'}
 
 
 export default LocationScreen
